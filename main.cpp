a58b21978c45c19290e09a6caccf2f08333c01f0
Go Watanabe <1570475+wtnbgo@users.noreply.github.com>
2024-11-11 20:37:20 +0900

    64bit link

--- main.cpp
+++ main.cpp
@@ -126,14 +126,28 @@ class tWMRStopFunction : public tTJSDispatch
 
 
 //---------------------------------------------------------------------------
-#pragma argsused
+
+#define EXPORT(hr) extern "C" __declspec(dllexport) hr __stdcall
+
+#ifdef _MSC_VER
+# if defined(_M_AMD64) || defined(_M_X64)
+#  pragma comment(linker, "/EXPORT:V2Link")
+#  pragma comment(linker, "/EXPORT:V2Unlink")
+# else
+#pragma comment(linker, "/EXPORT:V2Link=_V2Link@4")
+#pragma comment(linker, "/EXPORT:V2Unlink=_V2Unlink@0")
+#endif
+#endif
+
+extern "C"
 int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void* lpReserved)
 {
 	return 1;
 }
 //---------------------------------------------------------------------------
 static tjs_int GlobalRefCountAtInit = 0;
-extern "C" __declspec(dllexport) HRESULT __stdcall V2Link(iTVPFunctionExporter *exporter)
+
+EXPORT(HRESULT) V2Link(iTVPFunctionExporter *exporter)
 {
 	// スタブの初期化(必ず記述する)
 	TVPInitImportStub(exporter);
@@ -205,7 +219,8 @@ extern "C" __declspec(dllexport) HRESULT __stdcall V2Link(iTVPFunctionExporter *
 	return S_OK;
 }
 //---------------------------------------------------------------------------
-extern "C" __declspec(dllexport) HRESULT _stdcall V2Unlink()
+
+EXPORT(HRESULT)  V2Unlink()
 {
 	// 吉里吉里側から、プラグインを解放しようとするときに呼ばれる関数。
 
